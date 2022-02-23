76031642bc78724fa99b089e54abf5373a741198
Nishan <nishanbeleri@gmail.com>
2022-02-23 23:31:53 +0530

    ADD: Helpers

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
-//middlewares
+//regular middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookie and file middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+//swagger docs
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//morgan middleware
+app.use(morgan("tiny"));
 
 //import all routes
 const home = require("./routes/home");
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,15 @@
-exports.home = (req, res) => {
+const customPromise = require("../middlewares/customPromise");
+
+exports.home = customPromise(async (req, res) => {
 	res.status(200).json({
 		success: true,
 		message: "Hello from API",
 	});
-};
+});
 
-exports.homeDummy = (req, res) => {
+exports.homeDummy = customPromise((req, res) => {
 	res.status(200).json({
 		success: true,
 		message: "Dummy API Endpoint",
 	});
-};
+});
